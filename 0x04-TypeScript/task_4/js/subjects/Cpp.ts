e5cc9f39c7c764e5e938f53f2300594f33ff9f22
D0nG4667 <D0nG4667@users.noreply.github.com>
2025-11-20 21:38:17 +0300

    fix: checker req

--- 0x04-TypeScript/task_4/js/subjects/Cpp.ts
+++ 0x04-TypeScript/task_4/js/subjects/Cpp.ts
@@ -1,10 +1,13 @@
+/// <reference path="./Teacher.ts" />
+/// <reference path="./Subject.ts" />
+
 namespace Subjects {
     // Declaration merging: add optional experienceTeachingC to Teacher
     export interface Teacher {
         experienceTeachingC?: number;
     }
 
-    export class Cpp extends Subject {
+    export class Cpp extends Subjects.Subject {
         getRequirements(): string {
             return 'Here is the list of requirements for Cpp';
         }
--- 0x04-TypeScript/task_4/js/subjects/Subject.ts
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -1,3 +1,5 @@
+/// <reference path="Teacher.ts" />
+
 namespace Subjects {
   // ✅ Interface Subject with setTeacher signature
   export interface Subject {
